3387211510cde0ebaf927ff5c66754227b7c283c
muchhappy <shota-hirose@gmo.jp>
2018-07-21 00:37:44 +0900

    main

---
+++ Assets/Script/Button.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("CreateTask");
+        }
+    }
+}
+
+//これがそちらから見えていればOK
+//青木：新しくブランチを作ってから名前と適当なコメントをつけてPush
+//してみてね↓
